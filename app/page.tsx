c048820ff3e67624749ba0f59d6f44f7cab9c5a5
olafisoyevince <olafisoyevincent@gmail.com>
2024-03-01 17:57:15 +0100

    feat: added a canvas to the project so we can create and move shapes

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,52 @@
 "use client";
 
+import { fabric } from "fabric";
+
 import LeftSideBar from "@/components/LeftSideBar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import RightSideBar from "@/components/RightSideBar";
+import { useEffect, useRef } from "react";
+import {
+    handleCanvasMouseDown,
+    handleResize,
+    initializeFabric,
+} from "@/lib/canvas";
 
 export default function Page() {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const fabricRef = useRef<fabric.Canvas | null>(null);
+    const isDrawing = useRef(false);
+    const shapeRef = useRef<fabric.Object>(null);
+    const selectedShapeRef = useRef<string | null>("rectangle");
+
+    useEffect(() => {
+        const canvas = initializeFabric({ canvasRef, fabricRef });
+
+        canvas.on("mouse:down", (options) => {
+            handleCanvasMouseDown({
+                canvas,
+                options,
+                isDrawing,
+                shapeRef,
+                selectedShapeRef,
+            });
+        });
+
+        window.addEventListener("resize", () => {
+            handleResize({ canvas: fabricRef.current });
+        });
+
+        return () => {};
+    }, []);
+
     return (
         <main className=" h-screen overflow-hidden">
             <Navbar />
 
             <section className=" flex h-full flex-row">
                 <LeftSideBar />
-                <Live />
+                <Live canvasRef={canvasRef} />
                 <RightSideBar />
             </section>
         </main>
