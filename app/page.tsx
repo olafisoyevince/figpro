308c567fa695c4b580c48c9c5da08e071eb817c2
olafisoyevince <olafisoyevincent@gmail.com>
2024-03-02 15:14:59 +0100

    feat: added a delete and a refresh functionality to the canvas

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,17 @@ import RightSideBar from "@/components/RightSideBar";
 import { useEffect, useRef, useState } from "react";
 import {
     handleCanvasMouseDown,
+    handleCanvasMouseUp,
+    handleCanvasObjectModified,
+    handleCanvaseMouseMove,
     handleResize,
     initializeFabric,
+    renderCanvas,
 } from "@/lib/canvas";
 import { ActiveElement } from "@/types/type";
+import { useMutation, useStorage } from "@/liveblocks.config";
+import { defaultNavElement } from "@/constants";
+import { handleDelete } from "@/lib/key-events";
 
 export default function Page() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -20,6 +27,20 @@ export default function Page() {
     const isDrawing = useRef(false);
     const shapeRef = useRef<fabric.Object>(null);
     const selectedShapeRef = useRef<string | null>("rectangle");
+    const activeObjectRef = useRef<fabric.Object>(null);
+
+    const canvasObjects = useStorage((root) => root.canvasObjects);
+
+    const syncShapeInStorage = useMutation(({ storage }, object) => {
+        if (!object) return;
+        const { objectId } = object;
+        const shapeData = object.toJSON();
+        shapeData.objectId = objectId;
+
+        const canvasObjects = storage.get("canvasObjects");
+
+        canvasObjects.set(objectId, shapeData);
+    }, []);
 
     const [activeElement, setActiveElement] = useState<ActiveElement>({
         name: "",
@@ -27,8 +48,44 @@ export default function Page() {
         icon: "",
     });
 
+    const deleteAllShapes = useMutation(({ storage }) => {
+        const canvasObjects = storage.get("canvasObjects");
+
+        if (!canvasObjects || canvasObjects.size === 0) {
+            return true;
+        }
+
+        for (const [key, value] of canvasObjects.entries()) {
+            canvasObjects.delete(key);
+        }
+
+        return canvasObjects.size === 0;
+    }, []);
+
+    const deleteShapeFromStorage = useMutation(({ storage }, objectId) => {
+        const canvasObjects = storage.get("canvasObjects");
+
+        canvasObjects.delete(objectId);
+    }, []);
+
     const handleActiveElement = (elem: ActiveElement) => {
         setActiveElement(elem);
+
+        switch (elem?.value) {
+            case "reset":
+                deleteAllShapes();
+                fabricRef.current?.clear();
+                setActiveElement(defaultNavElement);
+                break;
+
+            case "delete":
+                handleDelete(fabricRef.current as any, deleteShapeFromStorage);
+                setActiveElement(defaultNavElement);
+                break;
+
+            default:
+                break;
+        }
 
         selectedShapeRef.current = elem?.value as string;
     };
@@ -46,12 +103,52 @@ export default function Page() {
             });
         });
 
+        canvas.on("mouse:move", (options) => {
+            handleCanvaseMouseMove({
+                canvas,
+                options,
+                isDrawing,
+                shapeRef,
+                selectedShapeRef,
+                syncShapeInStorage,
+            });
+        });
+
+        canvas.on("mouse:up", (options) => {
+            handleCanvasMouseUp({
+                canvas,
+                isDrawing,
+                shapeRef,
+                selectedShapeRef,
+                syncShapeInStorage,
+                setActiveElement,
+                activeObjectRef,
+            });
+        });
+
+        canvas.on("object:modified", (options) => {
+            handleCanvasObjectModified({
+                options,
+                syncShapeInStorage,
+            });
+        });
+
         window.addEventListener("resize", () => {
             handleResize({ canvas: fabricRef.current });
         });
 
-        return () => {};
+        return () => {
+            canvas.dispose();
+        };
     }, []);
+
+    useEffect(() => {
+        renderCanvas({
+            fabricRef,
+            canvasObjects,
+            activeObjectRef,
+        });
+    }, [canvasObjects]);
 
     return (
         <main className=" h-screen overflow-hidden">
