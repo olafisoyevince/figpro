5d953e716a40b0429ad2583fdc68039e2721dcee
olafisoyevince <olafisoyevincent@gmail.com>
2024-03-02 17:16:17 +0100

    feat: worked on an image upload functionality for the canvas

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ import { ActiveElement } from "@/types/type";
 import { useMutation, useRedo, useStorage, useUndo } from "@/liveblocks.config";
 import { defaultNavElement } from "@/constants";
 import { handleDelete, handleKeyDown } from "@/lib/key-events";
+import { handleImageUpload } from "@/lib/shapes";
 
 export default function Page() {
     const undo = useUndo();
@@ -30,7 +31,8 @@ export default function Page() {
     const isDrawing = useRef(false);
     const shapeRef = useRef<fabric.Object>(null);
     const selectedShapeRef = useRef<string | null>(null);
-    const activeObjectRef = useRef<fabric.Object>(null);
+    const activeObjectRef = useRef<fabric.Object | null>(null);
+    const imageInputRef = useRef<HTMLInputElement>(null);
 
     const canvasObjects = useStorage((root) => root.canvasObjects);
 
@@ -84,6 +86,15 @@ export default function Page() {
             case "delete":
                 handleDelete(fabricRef.current as any, deleteShapeFromStorage);
                 setActiveElement(defaultNavElement);
+                break;
+
+            case "image":
+                imageInputRef.current?.click();
+                isDrawing.current = false;
+
+                if (fabricRef.current) {
+                    fabricRef.current.isDrawingMode = false;
+                }
                 break;
 
             default:
@@ -169,6 +180,17 @@ export default function Page() {
             <Navbar
                 activeElement={activeElement}
                 handleActiveElement={handleActiveElement}
+                imageInputRef={imageInputRef}
+                handleImageUpload={(e: any) => {
+                    e.stopPropagation();
+
+                    handleImageUpload({
+                        file: e.target.files[0],
+                        canvas: fabricRef as any,
+                        shapeRef,
+                        syncShapeInStorage,
+                    });
+                }}
             />
 
             <section className=" flex h-full flex-row">
